ff9b4739013eea283ca154f48a36c50386764108
geethuj <geethuj@qburst.com>
2016-05-20 12:34:38 +0530

    Included smooth scroll in the arallax site

--- js/script.js
+++ js/script.js
@@ -79,18 +79,6 @@ $( document ).ready(function() {
 $(".countdown").countdown({
   date: "2 february 2017 6:19:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
   format: "on" // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
-},
-
-function() {
-
-  // the code here will run when the countdown ends
-  alert("done!")
-
-});
-  
-
-
-
-
-
+}
+)
 });
