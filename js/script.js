2db2d85da53671b8c50a1b3fc4be4be992130e99
Lavya R <lavya@qburst.com>
2016-05-25 19:40:04 +0530

    text slider completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     // Init Skrollr
     var s = skrollr.init();
 
@@ -98,5 +97,52 @@ $(document).ready(function() {
         format: "on" // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
     });
     $('#content').addClass('active');
+    
+});
+
+jQuery(document).ready(function ($) {
+
+  $('#checkbox').change(function(){
+    setInterval(function () {
+        moveRight();
+    }, 3000);
+  });
+
+	var slideCount = $('#slider ul li').length;
+	var slideWidth = $('#slider ul li').width();
+	var slideHeight = $('#slider ul li').height();
+	var sliderUlWidth = slideCount * slideWidth;
+
+	$('#slider').css({ width: slideWidth, height: slideHeight });
+
+	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
 
 });
